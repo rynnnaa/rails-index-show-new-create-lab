a7e5dbbb54abea76236e7d132001d0043297d67d
Ryna <pinktulips@Hotmail.com>
2021-03-30 11:40:01 -0700

    working on unitialized contsant error

---
+++ db/migrate/001_create_coupon.rb
@@ -0,0 +1,8 @@
+class CreateCoupon < ActiveRecord::Migration
+  def change
+    create_table :coupons do |t|
+      t.string :coupon_code, null: false
+      t.string :store, null: false
+    end
+  end
+end
