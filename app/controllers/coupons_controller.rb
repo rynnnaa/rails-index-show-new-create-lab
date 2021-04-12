891cef9bd72046868a67408dd868f230e80e9320
Ryna <pinktulips@Hotmail.com>
2021-04-12 14:17:23 -0700

    done

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,11 +13,9 @@ class CouponsController < ApplicationController
   end
 
   def create
-    @coupon = Coupon.new
-    @coupon.coupon_code = params[:coupon_code]
-    @coupon.store = params[:store]
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
     @coupon.save
-    redirect_to coupon_path(@coupon)
+    redirect_to @coupon
   end
 end
 
